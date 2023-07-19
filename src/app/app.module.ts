a3a5a8e970007c93318bea56c2caa31c53f1685f
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-19 12:31:18 -0700

    renamed header to sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainTitleComponent } from './main-title/main-title.component';
 import { SummaryComponent } from './summary/summary.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -15,11 +15,10 @@ import { FooterComponent } from './footer/footer.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { MatSidenavModule } from '@angular/material/sidenav';
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+    SidebarComponent,
     MainTitleComponent,
     SummaryComponent,
     SkillsComponent,
