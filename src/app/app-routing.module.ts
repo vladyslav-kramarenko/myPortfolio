f56cce0657c740c5d07afba3eb78f81cade85715
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-19 22:01:30 -0700

    changed routing links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MainTitleComponent } from './main-title/main-title.component';
-import { SummaryComponent } from './summary/summary.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { EducationComponent } from './education/education.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainComponent} from "./main/main.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: MainTitleComponent },
-  { path: 'summary', component: SummaryComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'experience', component: ExperienceComponent },
-  { path: 'education', component: EducationComponent },
-  { path: 'contacts', component: ContactsComponent },
-  { path: 'portfolio', component: PortfolioComponent }
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: MainComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
