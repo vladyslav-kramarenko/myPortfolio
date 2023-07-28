7f30caca9c353b7c8571904b883490ddca3f9eb2
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-07-28 10:55:27 -0700

    moved summary text to json

--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+interface Summary {
+  paragraphs: string[];
+}
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
+  summary!: Summary;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<Summary>('assets/data/summary.json').subscribe(data => {
+      this.summary = data;
+    });
+  }
 }
