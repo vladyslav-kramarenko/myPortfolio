2f2fc90c09b742df612fe62c8d45c1f4674c721e
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-12-04 11:08:01 -0800

    added page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from "./main/main.component";
 import {PortfolioItemComponent} from "./portfolio-item/portfolio-item.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'home', component: MainComponent},
   {path: 'portfolio/:id', component: PortfolioItemComponent},
+  {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
